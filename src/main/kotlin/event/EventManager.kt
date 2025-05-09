56ff6833a9e9d8c88c883eb51b3361adbc9915d4
aquestry <antonkonig691@gmail.com>
2025-05-09 21:10:34 +0200

    remove player spawn event with particles

--- src/main/kotlin/event/EventManager.kt
+++ src/main/kotlin/event/EventManager.kt
@@ -2,17 +2,14 @@ package net.eupixel.event
 
 import net.eupixel.event.events.AsyncPlayerConfiguration
 import net.eupixel.event.events.PlayerBlockBreak
-import net.eupixel.event.events.PlayerSpawn
 import net.minestom.server.event.GlobalEventHandler
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 import net.minestom.server.event.player.PlayerBlockBreakEvent
-import net.minestom.server.event.player.PlayerSpawnEvent
 import net.minestom.server.instance.InstanceContainer
 
 class EventManager(globalEventHandler: GlobalEventHandler, instanceContainer: InstanceContainer) {
     init {
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent::class.java) { AsyncPlayerConfiguration(it, instanceContainer) }
         globalEventHandler.addListener(PlayerBlockBreakEvent::class.java, ::PlayerBlockBreak)
-        globalEventHandler.addListener(PlayerSpawnEvent::class.java, ::PlayerSpawn)
     }
 }
