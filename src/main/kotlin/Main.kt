512de3e773721c15945c4db363e1edf12cbf31c5
aquestry <antonkonig691@gmail.com>
2025-05-10 23:27:55 +0200

    move unzip to vivlib, working

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,28 +6,7 @@ import net.minestom.server.instance.anvil.AnvilLoader
 import net.eupixel.event.EventManager
 import net.eupixel.vivlib.util.WebDavClient
 import net.minestom.server.extras.MojangAuth
-import java.io.File
 import java.io.FileOutputStream
-import java.util.zip.ZipInputStream
-
-fun unzip(zipFile: String, outputDir: String) {
-    ZipInputStream(File(zipFile).inputStream()).use { zin ->
-        val targetDir = File(outputDir).absoluteFile
-        var entry = zin.nextEntry
-        while (entry != null) {
-            val name = entry.name.substringAfter('/', "")
-            if (name.isNotEmpty()) {
-                val outFile = File(targetDir, name)
-                if (entry.isDirectory) outFile.mkdirs() else {
-                    outFile.parentFile!!.mkdirs()
-                    FileOutputStream(outFile).use { os -> zin.copyTo(os) }
-                }
-            }
-            zin.closeEntry()
-            entry = zin.nextEntry
-        }
-    }
-}
 
 fun startServer() {
     val server = MinecraftServer.init()
@@ -39,12 +18,10 @@ fun startServer() {
     server.start("0.0.0.0", 25565)
 }
 
-
-
 fun main(): Unit = runBlocking {
     val client = WebDavClient()
     val data = client.awaitFile("lobby.zip")
     FileOutputStream("lobby.zip").use { it.write(data) }
-    unzip("lobby.zip", "lobby")
+    client.unzip("lobby.zip", "lobby")
     startServer()
 }
